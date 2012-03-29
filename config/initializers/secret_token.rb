b720747118eb29c840b5421d42d577719ff1c33c
Andrey Fadeyev <mass1veffect@gmail.com>
2012-03-29 14:47:05 +0100

    Rails initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FadeyevCoUk::Application.config.secret_token = 'ff9c0318c11cb1bda378eb85e30220cc5be14f0bd597eb15d4f7dac25c50413ba479f6f50df61a3345ff492390142567c4625ff800d44f1eda507f3394cc50a1'
